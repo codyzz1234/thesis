13723c8ead92374d4bfac67aef1262614944e72f
cody <kenleybeast1@gmail.com>
2021-12-23 16:04:53 +0800

    Continued working on card

--- application/views/DashBoardView.php
+++ application/views/DashBoardView.php
@@ -14,22 +14,39 @@
                     <!-- Content Row -->
                     <div class="row">
                     <div class="card-deck dashBoardDeck">
-                        <div class="card">
-                            <img class="card-img-top" src="..." alt="Card image cap">
+                        
+                       
+                        <div class="card text-white bg-primary mb-3">
+                            <div class="card-header">Total Number of Employees</div>
                             <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                <div class="row">
+                                    <div class="col">
+                                        <h1 class="card-title">100</h2>
+                                    </div>
+                                    <div class="col">
+                                        <i class="fas fa-user-alt fa-5x"></i>    
+                                    </div>
+                                </div>
+                                
                             </div>
                         </div>
+                        
+                        
+
+                        
+
+
+
                         <div class="card">
-                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <i class="fas fa-user-alt fa-5x"></i>    
                             <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
+
+
                         <div class="card">
                             <img class="card-img-top" src="..." alt="Card image cap">
                             <div class="card-body">
