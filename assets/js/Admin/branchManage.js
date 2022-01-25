c99e671eb264bccd67c4b4caac27acb1acbaafba
cody <kenleybeast1@gmail.com>
2022-01-25 22:28:22 +0800

    Added data to branchmanage table

--- assets/js/Admin/branchManage.js
+++ assets/js/Admin/branchManage.js
@@ -38,13 +38,91 @@ $(document).ready(function () {
 
                 }
                 else{
-                    toastr["success"]("Alert",data.message);
+                    var table;
                     setters = data.posts;
                     console.log(setters);
+                    if($.fn.dataTable.isDataTable('#branchTable')) {
+                        console.log("drew it!");
+                        table = $('#branchTable').DataTable();
+                        table.clear().draw();
+                        table.rows.add(setters); // Add new data
+                        table.columns.adjust().draw();
+                    }
+                    else{
+                        console.log("generate it");
+                        table = $('#branchTable').DataTable({
+                            destroy:true,
+                            response:true,
+                            dom: //'Blfrtip', // if you remove this line you will see the show entries dropdown
+                            'B'+ 
+
+                            "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>"+
+                             "rtip",
+
+                            buttons: [
+                               // 'copy', 'csv', 'excel', 'pdf', 'print',
+                               {
+                                   text:'Copy Table to Clipboard',
+                                   className: "copy spaceButtons",
+                                   extend:'copy',
+                               },
+                               {
+                                   text:'Export Table To Excel',
+                                   extend:'excel',
+                                   className:"excel spaceButtons"
+                               },
+                               {
+                                   text:'Export Table To CSV',
+                                   extend:'csv',
+                                   className:"csv spaceButtons"
+                               },
+                               
+                               {
+                                    text:'Export Table To PDF',
+                                    extend:'pdf',
+                                    className:"pdf spaceButtons",
+                                    orientation : 'landscape',
+                                    pageSize : 'LEGAL',
+                               },
+    
+                            ],
+                            data:data.posts,
+                            columns:[
+                                {
+                                    title:"Branch",
+                                    data:"Branch",
+                                    width:"5%"
+                                },
+                                {
+                                    title:"Address",
+                                    width:"10%",
+                                    data:"Address"
+                                    
+                                },
+                                {
+                                    title:"Number Of Employees",
+                                    data:"NumEmp",
+                                    width:"10%"
+                                },
+                                {
+                                    title:"Actions",
+                                    data:null,
+                                    width:"5%",
+                                    render:function(type,data,meta,row){
+                                        var editButton = '<a href="#" value = "'+'" class = "btn btn-outline-info editButton"><i class="fas fa-pen-square"></i></a>'
+                                        var deleteButton = '<a href="#" value = "'+'" class = "btn btn-outline-danger deleteButton"><i class="fas fa-trash-alt"></i></a>'
+                                        return editButton+deleteButton
+                                    }
+                                }
+                            ],
+    
+
+                        })
+
+                    }
                 }
             }
         });
-
     }
 
 });
