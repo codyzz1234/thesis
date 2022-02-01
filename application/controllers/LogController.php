79ae88b61f82ca0ebe197371d4d2e8f650301eec
cody <kenleybeast1@gmail.com>
2022-02-01 23:34:31 +0800

    Added LogController and Log View

---
+++ application/controllers/LogController.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LogController extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URLP
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->library('session');
+	}
+
+	public function index()
+	{
+		$sessionData = $this->session->userdata('username');
+		if($sessionData != ''){
+			$data = array(
+				'username' => $sessionData,
+                'adminId' => $this->session->userdata('adminId')
+
+			);
+			$this->load->view('template/DashBoardHead',$data);
+			$this->load->view('ActivityLogView',$data);
+		}
+		else{
+			redirect('Login');
+		}
+	}
+
+    public function feth()
+    {
+
+    }
+}
